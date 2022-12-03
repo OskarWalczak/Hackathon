1b31e6b94100c72433d1c4ba8a71e236fe8f9a75
Marcin <Marcin@hacka.ton>
2022-12-03 01:02:21 +0100

    Screen fading and powering back up

---
+++ Assets/Scripts/FadingCtrl.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadingCtrl : MonoBehaviour
+{
+
+    private Image m_FadeScreen;
+    private float m_FadingSpeed = 0.005f;
+    private float m_RechargeRate = 0.1f;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        m_FadeScreen = GameObject.Find("Image").GetComponent<Image>();
+        Color backgroundColor = m_FadeScreen.color;
+        backgroundColor.a = 0;
+        m_FadeScreen.color = backgroundColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        {
+            Color backgroundColor = m_FadeScreen.color;
+            float newAlpha = Mathf.Min(1, backgroundColor.a + m_FadingSpeed);
+            backgroundColor.a = newAlpha;
+            m_FadeScreen.color = backgroundColor;
+        }
+
+        if(Input.GetKey("c") && Input.GetKey("p"))
+        {
+            Color backgroundColor = m_FadeScreen.color;
+            float newAlpha = Mathf.Max(0, backgroundColor.a - m_RechargeRate);
+            backgroundColor.a = newAlpha;
+            m_FadeScreen.color = backgroundColor;
+        }
+    }
+}
