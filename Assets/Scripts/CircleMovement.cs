85f33c92ce63043074ef9f4859a17c775f1f8d58
OskarWalczak <owalczak@bee2code.com>
2022-12-03 02:45:16 +0100

    Death added

--- Assets/Scripts/CircleMovement.cs
+++ Assets/Scripts/CircleMovement.cs
@@ -14,6 +14,9 @@ public class CircleMovement : MonoBehaviour
     public float groundCheckRadius;
     public LayerMask groundLayer;
 
+    public bool shouldDie;
+    public LayerMask deathLayer;
+
     // Start is called before the first frame update
     private void Awake()
     {
@@ -27,8 +30,18 @@ public class CircleMovement : MonoBehaviour
 
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
+        shouldDie = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, deathLayer);
+
         if(isGrounded && (Input.GetKey(KeyCode.Space) || Input.GetAxis("Vertical")>0)){
             body.AddForce(Vector2.up, ForceMode2D.Impulse);
         }
+
+        if(shouldDie){
+            die();
+        }
+    }
+
+    private void die(){
+        body.position = new Vector2(-8, 8);
     }
 }
