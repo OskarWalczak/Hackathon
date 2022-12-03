05b98a873697674af7d7556dff330172882e6d77
Marcin <Marcin@hacka.ton>
2022-12-03 16:32:19 +0100

    Player light fadding

--- Assets/Scripts/FadingCtrl.cs
+++ Assets/Scripts/FadingCtrl.cs
@@ -2,39 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Rendering.Universal;
 
 public class FadingCtrl : MonoBehaviour
 {
 
-    private Image m_FadeScreen;
-    private float m_FadingSpeed = 0.005f;
+    private Light2D playerLight;
+    private float m_FadingSpeed = 0.0005f;
     private float m_RechargeRate = 0.1f;
+    private float m_InnerRadiusFadingSpeed = 0.02f;
+    private float m_InnerRadiusRechargeRate = 1f;
+    private float m_OuterRadiusFadingSpeed = 0.06f;
+    private float m_OuterRadiusRechargeRate = 3f;
 
     // Start is called before the first frame update
     private void Awake()
     {
-        m_FadeScreen = GameObject.Find("Image").GetComponent<Image>();
-        Color backgroundColor = m_FadeScreen.color;
-        backgroundColor.a = 0;
-        m_FadeScreen.color = backgroundColor;
+        playerLight = GetComponent<Light2D>();
+        playerLight.intensity = 1;
+        playerLight.pointLightInnerRadius = 20;
+        playerLight.pointLightOuterRadius = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
-        {
-            Color backgroundColor = m_FadeScreen.color;
-            float newAlpha = Mathf.Min(1, backgroundColor.a + m_FadingSpeed);
-            backgroundColor.a = newAlpha;
-            m_FadeScreen.color = backgroundColor;
-        }
-
+        playerLight.intensity = Mathf.Max(0, playerLight.intensity - m_FadingSpeed);
+        playerLight.pointLightInnerRadius = Mathf.Max(0, playerLight.pointLightInnerRadius - m_InnerRadiusFadingSpeed);
+        playerLight.pointLightOuterRadius = Mathf.Max(0, playerLight.pointLightOuterRadius - m_OuterRadiusFadingSpeed);
+        
         if(Input.GetKey("c") && Input.GetKey("p"))
-        {
-            Color backgroundColor = m_FadeScreen.color;
-            float newAlpha = Mathf.Max(0, backgroundColor.a - m_RechargeRate);
-            backgroundColor.a = newAlpha;
-            m_FadeScreen.color = backgroundColor;
+        {        
+            playerLight.intensity = Mathf.Min(1, playerLight.intensity + m_RechargeRate);
+            playerLight.pointLightInnerRadius = Mathf.Min(20, playerLight.pointLightInnerRadius + m_InnerRadiusRechargeRate);
+            playerLight.pointLightOuterRadius = Mathf.Min(60, playerLight.pointLightOuterRadius + m_OuterRadiusRechargeRate);
         }
     }
 }
