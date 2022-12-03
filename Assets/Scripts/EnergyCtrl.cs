daee3ebd857d76c5548134c3264f1c3633ba76b3
Marcin <Marcin@hacka.ton>
2022-12-03 13:37:57 +0100

    HUD energy bar (and text)

--- Assets/Scripts/EnergyCtrl.cs
+++ Assets/Scripts/EnergyCtrl.cs
@@ -6,13 +6,15 @@ using UnityEngine.UI;
 public class EnergyCtrl : MonoBehaviour
 {
     private CircleMovement player;
-    private float m_EnergyDrainSpeed = 0.005f;
-    private float m_EnergyRechargeRate = 1f;
+    private Slider slider;
+    private float m_EnergyDrainSpeed = 0.0005f;
+    private float m_EnergyRechargeRate = 0.1f;
 
     // Start is called before the first frame update
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<CircleMovement>();
+        slider = GameObject.Find("EnergySlider").GetComponent<Slider>();
     }
 
     // Update is called once per frame
@@ -23,6 +25,6 @@ public class EnergyCtrl : MonoBehaviour
         {
             player.m_Energy =  Mathf.Min(1.2f, player.m_Energy + m_EnergyRechargeRate);
         }
-
+        slider.value = player.m_Energy;
     }
 }
